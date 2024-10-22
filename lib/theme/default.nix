a2488069eb25301213848951e406c9b5956b2042
dtgagnon <82989011+dtgagnon@users.noreply.github.com>
2024-10-21 22:26:43 -0400

    Add centralized theme selection and update colorscheme
    * **lib/theme/default.nix**: Add file to define `selectedTheme` variable and dynamically import the corresponding theme file.
    * **modules/nixvim/colorscheme/default.nix**: Update to reference the centralized theme selection variable and use the imported theme.

---
+++ lib/theme/default.nix
@@ -0,0 +1,11 @@
+{ lib, ... }:
+
+let
+  selectedTheme = "everforest"; # Change this to switch themes
+
+  themeFile = builtins.readFile (./ + selectedTheme + "/default.nix");
+  theme = import (./ + selectedTheme + "/default.nix") { inherit lib; };
+in
+{
+  inherit theme;
+}
--- modules/nixvim/colorscheme/default.nix
+++ modules/nixvim/colorscheme/default.nix
@@ -1,30 +1,10 @@
-{ helpers, ... }: {
-  # extraConfigLuaPost = # lua
-  #   ''
-  #     do
-  #       -- The NixVim Nord colorscheme doesn't assign a pleasing color to the window
-  #       -- separator by default. Here we override it with a softer color.
-  #       local colors = require("nord.colors")
-  #       vim.api.nvim_set_hl(0,  "WinSeparator", {
-  #         fg = colors.nord2_gui,
-  #       })
-  #       vim.api.nvim_set_hl(0,  "LspInlayHint", {
-  #         fg = colors.nord3_gui,
-  #       })
-  #     end
-  #   '';
-  #
-  # colorschemes.nord = {
-  #   enable = true;
-  #   settings = {
-  #     borders = true;
-  #     contrast = false;
-  #     disable_background = false;
-  #     enable_sidebar_background = true;
-  #   };
-  # };
+{ helpers, lib, ... }:
 
-  colorschemes.everforest = {
+let
+  theme = lib.spirenix.theme;
+in
+{
+  colorschemes.${theme} = {
     enable = true;
     settings = {
       borders = true;
